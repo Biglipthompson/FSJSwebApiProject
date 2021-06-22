02059364c81200e05458daadfdbc6915884dcd36
Ja'Quan Thompson <jaquanthompson87@icloud.com>
2021-06-22 01:03:59 -0400

    Commit
    Commit

--- public_api_request-v1/js/script.js
+++ public_api_request-v1/js/script.js
@@ -7,6 +7,12 @@ email, location, phone, dob &noinfo &nat=US`
 const gallery = document.querySelector('.gallery');
 const body = document.querySelector('body');
 
+fetch(urlAPI)
+.then(res => res.json())
+.then(res => res.results)
+.then(displayEmployees)
+.catch(err => console.log(err))
+
 //Markup for the search box 
 const searchHtml = `
 <form action="#" method="get">
@@ -16,14 +22,6 @@ const searchHtml = `
 `;
 searchDiv.insertAdjacentHTML('beforeend', searchHtml);
 
-
-
-fetch(urlAPI)
-.then(res => res.json())
-.then(res => res.results)
-.then(displayEmployees)
-.catch(err => console.log(err))
-
 function displayEmployees(employeeData) {
 employees = employeeData;
 
@@ -32,10 +30,12 @@ let employeeHTML = '';
 
 //this loops through all the employee info and adds it to the html for that person
 employees.forEach((employee, index) => {
-    let name = employee.name;
-    let email = employee.email;
-    let city = employee.location.city;
-    let picture = employee.picture;
+let name = employee.name;
+let email = employee.email;
+let city = employee.location.city;
+let picture = employee.picture;
+
+
 
 employeeHTML += `
 <div class="card"> 
@@ -52,6 +52,47 @@ employeeHTML += `
     });
     gallery.innerHTML = employeeHTML;
 }
+
+function displayModal (index) {
+    let { name, dob, phone, email, 
+    location: {city, street, state, postcode
+    }, picture } = employees[index];
+    let date = new Date(dob.date);  
+
+     
+    const modalHTML = `
+    <img class="avatar" src="${picture.large}"/>
+    <div class="text-container">
+    <h2 class="name">${name.first} ${name.last}</h2>
+    <p class="email">${email}</p>
+    <p class="address">${city}</p>
+    <hr />
+
+
+    <p>${phone}</p>
+    <p> ${street.name}, ${street.number} ${state} ${postcode}</p>
+    <p>Birthday:${date.getMonth()}/${date.getDate()}/${date.getFullYear()}</p>
+    </div>
+`;
+
+overlay.classList.remove("hidden");
+modalContainer.innerHTML = modalHTML;
+}
+
+// This function checks if if the grid container itself was clicked or a child element 
+gallery.addEventListener('click', e => {
+    if (e.target !== gallery) {
+        // Select the card element based on its proximity to actual element clicked
+        const card = e.target.closest('.card');
+        const index = card.getAttribute('data-index');
+
+        displayModal(index);
+    } 
+});
+
+modalClose.addEventListener('click', () => {
+    overlay.classList.add('hidden');
+});
 
 
 

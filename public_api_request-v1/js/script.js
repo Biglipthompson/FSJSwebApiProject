a24e285d3608d5af3a628a6e9ddcfe64b7b9592a
Ja'Quan Thompson <jaquanthompson87@icloud.com>
2021-06-23 17:00:58 -0400

    Commit
    Commit

--- public_api_request-v1/js/script.js
+++ public_api_request-v1/js/script.js
@@ -5,7 +5,7 @@ const searchDiv = document.querySelector('.search-container');
 const urlAPI = `https://randomuser.me/api/?results=12&inc=name, picture,
 email, location, phone, dob &noinfo &nat=US`
 const gallery = document.querySelector('.gallery');
-const body = document.querySelector('body');
+// const body = document.querySelector('body');
 const modalClose = document.querySelectorAll('.modal-close-btn');
 const modalContainer = document.querySelector('.modal-container')
 
@@ -38,18 +38,20 @@ let email = employee.email;
 let city = employee.location.city;
 let picture = employee.picture;
 
-employeeHTML += `
-<div class="card" data-index="${index}">
-<img class="avatar" src="${picture.large}" />
-<div class="text-container">
+employeeHTML = `
+ <div class="card" data-index="${index}">
+ <div class="card-img-container">
+<img class="card-img" src="${picture.large}" alt="profile picture"/>
+</div>
+<div class="card-info-container">
 <h2 class="name">${name.first} ${name.last}</h2>
-<p class="email">${email}</p>
-<p class="address">${city}</p>
+<p class="card-text">${email}</p>
+<p class="card-text cap">${city.state}</p>
 </div>
 </div>
-`
-});
-    gallery.innerHTML = employeeHTML;
+`;
+gallery.insertAdjacentHTML("beforeend", employeeHTML);
+})
 }
 
 function displayModal(index) {
@@ -65,31 +67,29 @@ function displayModal(index) {
     <p class="email">${email}</p>
     <p class="address">${city}</p>
     <hr />
-
-
     <p>${phone}</p>
     <p> ${street.name}, ${street.number} ${state} ${postcode}</p>
     <p>Birthday:${date.getMonth()}/${date.getDate()}/${date.getFullYear()}</p>
     </div>
 `;
 
-modalContainer.classList.remove("hidden");
-modalClose.innerHTML = modalHtml;
+gallery.classList.remove("hidden");
+displayModal.innerHTML = modalHtml;
 }
 
 // This function checks if if the grid container itself was clicked or a child element 
-gallery.addEventListener('click', e => {
+gallery.addEventListener('click', (e) => {
     if (e.target !== gallery) {
         // Select the card element based on its proximity to actual element clicked
-        const card = e.target.closest('.card');
-        const index = card.getAttribute('data-index');
-
+        const clicked = e.target.closest(".card");
+        const index = clicked.getAttribute("data-index");
         displayModal(index);
     } 
 });
-modalClose.addEventListener('click', () => {
-    modalContainer.classList.add('hidden');
-});
+// gallery.addEventListener('click', () => {
+//     modalClose.className = 'hidden';
+// });
+
 
 
 
